1db3ee694cd3890eef212b10de534a029851964f
ZeetPatel2712 <zeetpatel2712@gmail.com>
2024-03-13 21:00:26 +0530

    update code

---
+++ 01_Introduction/src/App.jsx
@@ -0,0 +1,13 @@
+const name = "Hello Word";
+console.log("🚀 ~ name:", name);
+
+
+function App() {
+    return (
+        <>
+            <h1>Hello Code Aur Chai</h1>
+        </>
+    );
+}
+
+export default App;
